71f06b964fc35601fbba29f2adbc116aa111b70d
muraina david <muraina david>
2026-05-01 17:01:07 +0100

    i fixed the websocket broadcast issue, work can now progress to backend and data visualization code needs to be updated to unpack python struct data

--- client.py
+++ client.py
@@ -10,10 +10,10 @@ import asyncio
 from websockets.asyncio.client import connect
 async def get_data():
     async with connect("ws://localhost:4443") as websocket:
-        # message = await websocket.recv()
+        #message = await websocket.recv()
        # await websocket.send(json.dumps([interval, max_altitude, speed]))
         async for message in websocket:
-            print(struct.unpack("< i i i i h i B H h h h h h h H H ", message))
+            print(struct.unpack("< I i i i h H B H h h h h h h H H ", message), 34)
             await asyncio.sleep(0.02)
 try:
     asyncio.run(get_data())
